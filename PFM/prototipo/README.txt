1b286fc9c354a95e187c5424c1e288835ac1a4bd
eugenio <eugenio.arosteguy@fluxit.com.ar>
2017-06-16 16:05:41 -0300

    first commit

---
+++ PFM/prototipo/README.txt
@@ -0,0 +1,33 @@
+http://www.convertcsv.com/generate-test-data.htm
+
+Entry set format
+id, edad, sexo, salario, codPostal, tienePzoFijo
+
+* Random entry set (10 thounsand):
+    seq, age, gender, digit(5), digit(2), bool
+
+* Rich guys with tendency to have PZ (1 thousand)
+    seq, age, gender, digit(6), digit(2), TRUE
+
+* Rich guys with tendency to not have PZ (1 thousand)
+    seq, age, gender, digit(4), digit(2), FALSE
+
+
+Manual treatment: 
+    * Manually remove zeros at left
+    * replace boolean and gender by bit representation
+    * remove last line
+    
+
+
+ML
+    Coursera
+        gradiente descendiente
+        regresion lineal
+        regrsion logistica
+
+    prototip PFM logistic regresion 2 var completo
+    prototip PFM logistic regresion multivarible 
+
+    proximo redes neuronales
+    persistir los datos
---
+++ PFM/prototipo/multiFeature.m
@@ -0,0 +1,112 @@
+%% Machine Learning Online Class - Exercise 2: Logistic Regression
+%
+%  Instructions
+%  ------------
+% 
+%  This file contains code that helps you get started on the logistic
+%  regression exercise. You will need to complete the following functions 
+%  in this exericse:
+%
+%     sigmoid.m
+%     costFunction.m
+%     predict.m
+%     costFunctionReg.m
+%
+%  For this exercise, you will not need to change any code in this file,
+%  or any other files other than those mentioned above.
+%
+
+%% Initialization
+clear ; close all; clc
+
+%% Load Data
+%  The first two columns contains the exam scores and the third column
+%  contains the label.
+
+random = load('random.txt');
+poor = load('poor.txt');
+rich = load('rich.txt');
+young = load('young.txt');
+elder = load('elder.txt');
+men = load('hombre.txt');
+women = load('mujer.txt');
+platence = load('platence.txt');
+data = [random([1:40],:);poor;rich;young;elder;men;women;platence];
+X = data(:, [2:5]); y = data(:, 6);
+
+%% ============ Part 2: Compute Cost and Gradient ============
+%  In this part of the exercise, you will implement the cost and gradient
+%  for logistic regression. You neeed to complete the code in 
+%  costFunction.m
+
+%  Setup the data matrix appropriately, and add ones for the intercept term
+[m, n] = size(X);
+
+% Add intercept term to x and X_test
+X = [ones(m, 1) X];
+
+% Initialize fitting parameters
+initial_theta = zeros(n + 1, 1);
+
+% Compute and display initial cost and gradient
+[cost, grad] = costFunction(initial_theta, X, y);
+
+fprintf('Cost at initial theta (zeros): %f\n', cost);
+fprintf('Gradient at initial theta (zeros): \n');
+fprintf(' %f \n', grad);
+
+fprintf('\nProgram paused. Press enter to continue.\n');
+pause;
+
+
+%% ============= Part 3: Optimizing using fminunc  =============
+%  In this exercise, you will use a built-in function (fminunc) to find the
+%  optimal parameters theta.
+
+%  Set options for fminunc
+options = optimset('GradObj', 'on', 'MaxIter', 400);
+
+%  Run fminunc to obtain the optimal theta
+%  This function will return theta and the cost 
+[theta, cost] = ...
+	fminunc(@(t)(costFunction(t, X, y)), initial_theta, options);
+
+% Print theta to screen
+fprintf('Cost at theta found by fminunc: %f\n', cost);
+fprintf('theta: \n');
+fprintf(' %f \n', theta);
+
+fprintf('\nProgram paused. Press enter to continue.\n');
+pause;
+
+%% ============== Part 4: Predict and Accuracies ==============
+%  After learning the parameters, you'll like to use it to predict the outcomes
+%  on unseen data. In this part, you will use the logistic regression model
+%  to predict the probability that a student with score 45 on exam 1 and 
+%  score 85 on exam 2 will be admitted.
+%
+%  Furthermore, you will compute the training and test set accuracies of 
+%  our model.
+%
+%  Your task is to complete the code in predict.m
+
+%  Predict probability for a student with score 45 on exam 1 
+%  and score 85 on exam 2 
+
+newUsers = load('newUsers.txt');
+XToPredict = newUsers(:,[2:5]);
+XToPredict = [ones(size(XToPredict,1), 1) XToPredict];
+prob = sigmoid(XToPredict * theta);
+disp('For new users we predict having pzo fijo probability of \n');
+disp([newUsers, prob]);
+
+% Compute accuracy on our training set
+p = predict(theta, X);
+
+fprintf('Train Accuracy: %f\n', mean(double(p == y)) * 100);
+fprintf('\n');
+
+fprintf('People that must have pzo fijo:\n');
+disp(data(find(p&!y),:));
+
+fprintf('\n'); 
